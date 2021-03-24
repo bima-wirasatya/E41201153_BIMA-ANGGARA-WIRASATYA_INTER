34680c749c2b37887930d80626265a06d8a6c415
bima-wirasatya <75077630+bima-wirasatya@users.noreply.github.com>
2021-03-24 18:53:49 +0700

    Praktik Bkpm week2

---
+++ Praktik1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktik1;
+
+/**
+ *
+ * @author Bima
+ */
+public class Praktik1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Hello ");
+     System.out.println("World!");
+     
+     System.out.print("Hello ");
+     System.out.print("World!");
+    }
+    
+}
